15b29e14fdf2bbe4b67865efb59fb4b2004d20b5
Katushkahey <49131838+Katushkahey@users.noreply.github.com>
2023-07-25 23:15:40 +0300

    Update Book.java
    remove @Component from dto

--- src/main/java/com/homework/spring/dto/Book.java
+++ src/main/java/com/homework/spring/dto/Book.java
@@ -4,14 +4,12 @@ import com.homework.spring.entity.BookComment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Component
 public class Book {
 
     Long id;

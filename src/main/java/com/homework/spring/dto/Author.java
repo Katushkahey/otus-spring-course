cb954442557619e8cde1dfe0cc93ef7696fd7ae2
Katushkahey <49131838+Katushkahey@users.noreply.github.com>
2023-07-25 23:15:13 +0300

    Update Author.java
    remove @Component from dto

--- src/main/java/com/homework/spring/dto/Author.java
+++ src/main/java/com/homework/spring/dto/Author.java
@@ -3,14 +3,12 @@ package com.homework.spring.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Component
 public class Author {
 
     Long id;
